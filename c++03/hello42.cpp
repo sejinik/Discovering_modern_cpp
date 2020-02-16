a4b2474162afce4212ecf31051e33a64009f18e4
Sejin <sejinik@gmail.com>
2020-02-16 22:10:22 +0900

    vector_test

---
+++ c++03/hello42.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"The answer to Ultimate Question of Life, \n"
+             <<"the Universe, and Everything is:"
+             <<std::endl<<6*7<<std::endl;
+            
+    return 0;
+}
